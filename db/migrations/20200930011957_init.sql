feab6af56c567f3e4394cb67ba24665d2ae0ba7e
awalias <antwilson@hotmail.co.uk>
2020-11-24 14:44:55 +0800

    chore: formats init sql

--- db/migrations/20200930011957_init.sql
+++ db/migrations/20200930011957_init.sql
@@ -1,45 +1,45 @@
 -- migrate:up
-
-create extension if not exists "uuid-ossp" WITH SCHEMA extensions;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA extensions;
 
 -- READ DATA
-create table public.read (
-  id          uuid      default extensions.uuid_generate_v4()    not null primary key,
-  slug        uuid      default extensions.uuid_generate_v4()
+CREATE TABLE public.read (
+  id bigserial PRIMARY KEY,
+  slug int
 );
-comment on table public.read is 'Table with some data to test read benchmarking';
+
+COMMENT ON TABLE public.read IS 'Table with some data to test read benchmarking';
 
 -- WRITE DATA
-create table public.write (
-  id          uuid      default extensions.uuid_generate_v4()      not null primary key,
-  slug        uuid      default extensions.uuid_generate_v4()
+CREATE TABLE public.write (
+  id bigserial PRIMARY KEY,
+  slug int
 );
-comment on table public.write is 'Table with some data to test write benchmarking';
+
+COMMENT ON TABLE public.write IS 'Table with some data to test write benchmarking';
 
 -- BENCHMARKING-DATA
-create table public.benchmarks (
-  id            uuid    default extensions.uuid_generate_v4()  not null primary key,
+CREATE TABLE public.benchmarks (
+  id uuid DEFAULT extensions.uuid_generate_v4 () NOT NULL PRIMARY KEY,
   benchmark_name text,
-  data          jsonb,
-  inserted_at   timestamp with time zone    default timezone('utc'::text, now()) not null
+  data jsonb,
+  inserted_at timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL
 );
-comment on table public.benchmarks is 'Table to store benchmarking data';
+
+COMMENT ON TABLE public.benchmarks IS 'Table to store benchmarking data';
 
 -- BENCHMARKING SUMMARIES
-create or replace view benchmark_summaries as (
-  select
-    id
-  , benchmark_name
-  , data->'metrics'->'http_reqs'->'rate' as http_reqs_rate
-  , data->'metrics'->'http_reqs'->'count' as http_reqs_count
-  , data->'metrics'->'http_req_duration'->'avg' as http_req_duration_avg
-  , pg_size_pretty((data->'metrics'->'data_received'->'count')::bigint) as data_received
-  , pg_size_pretty((data->'metrics'->'data_sent'->'count')::bigint) as data_sent
-  , data->'metrics'->'vus'->'value' as vus
-  , data->'metrics'->'failed requests'->'value' as failed_requests
-  from
-  benchmarks
-);
+CREATE OR REPLACE VIEW benchmark_summaries AS (
+  SELECT
+    id,
+    benchmark_name,
+    data -> 'metrics' -> 'http_reqs' -> 'rate' AS http_reqs_rate,
+    data -> 'metrics' -> 'http_reqs' -> 'count' AS http_reqs_count,
+    data -> 'metrics' -> 'http_req_duration' -> 'avg' AS http_req_duration_avg,
+    pg_size_pretty((data -> 'metrics' -> 'data_received' -> 'count')::bigint) AS data_received,
+    pg_size_pretty((data -> 'metrics' -> 'data_sent' -> 'count')::bigint) AS data_sent,
+    data -> 'metrics' -> 'vus' -> 'value' AS vus,
+    data -> 'metrics' -> 'failed requests' -> 'value' AS failed_requests
+  FROM
+    benchmarks);
 
 -- migrate:down
-
