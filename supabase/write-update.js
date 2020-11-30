6b35f443bf12d1dda0e5d479f9ce8ef7e8034c78
awalias <antwilson@hotmail.co.uk>
2020-11-30 22:42:59 +0800

    fix: write update response value

--- supabase/write-update.js
+++ supabase/write-update.js
@@ -33,8 +33,8 @@ export default function () {
   }
   // we can use the 1 million row table 'read'
   const res = http.patch(`${supabaseUrl}/rest/v1/read?id=eq.${Math.floor(Math.random() * 1000000 + 1)}`, JSON.stringify(body), params)
-  myFailRate.add(res.status !== 201 && res.status !== 204)
-  if (res.status !== 201 && res.status !== 204) {
+  myFailRate.add(res.status > 399)
+  if (res.status > 399) {
     console.log(res.status)
   }
 }
